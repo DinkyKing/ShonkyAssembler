dfc0d335e58cdbd27f42eac1745b77f33401db34
ShonkyAssembler1 <joshua.crowell@students.mq.edu.au>
2016-07-17 13:38:59 +1000

    VB001
    Added notes and visual version number

---
+++ Assets/Notes.cs
@@ -0,0 +1,12 @@
+﻿//-------------------------------------------------//
+/*
+ * Just started
+ *
+ *Character (Made but not doing anything)
+ *Level Template(Working but bugged)
+ *Crane Working ( but doesnt scale graphicaly)
+ *Collision and block drops work ( But too much bounciness with the blocks)
+ *Game referee for points and level finish
+ * 
+ * 
+ *///-----------------------------------------------//
