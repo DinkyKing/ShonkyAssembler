e8695d9766f95fc7afbe3eeb731deb07937d69b4
Shonky <joshua.crowell@students.mq.edu.au>
2016-08-06 14:29:31 +1000

    computer

---
+++ Assets/GAME/Scripts/ScoreDisplayUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreDisplayUI : MonoBehaviour {
+
+   // public Text myScore = "Hello";
+    
+    void Update()
+    {
+        //myScore.Text = "Helloooo";
+    }
+}
